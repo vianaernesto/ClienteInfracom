6c4f75adb5f9716f41b488b3180ca0ee304ab7d2
vianaernesto <le.viana@uniandes.edu.co>
2019-09-29 14:19:10 -0500

    Hash

--- app/app.py
+++ app/app.py
@@ -7,6 +7,8 @@ ipMaquinaServidor = "13.92.208.121"
 archivo = 'archivo.png'
 cs = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
+h = hashlib.sha256()
+
 print("Cliente iniciado")
 ##Para probar localmente cambiar "ipMaquinaServidor" por socket.gethostname()
 cs.connect((socket.gethostname(),5000))
@@ -25,23 +27,36 @@ print("Recibiendo..")
 with open(archivo, 'wb') as fw:
     while True:
         ##Esto es para que no sea bloqueante
-        cs.settimeout(0.1)
+        cs.settimeout(1)
         try:
             data = cs.recv(1024)
+            h.update(data)
             fw.write(data)
             if not data:
                 t2 = time.time()*1000
+                cs.send(b"hash")
                 break
         except Exception as e:
             t2 = time.time()*1000
+            cs.send(b"hash")
             break
     fw.close()
-cs.send(b"Recibido")
-print("Recibido")
-
+print("Hash obtenido del archivo recibido",h.digest())
+hrecibido = cs.recv(1024)
+print("Hash recibido desde el servidor:  ",hrecibido)
+if(hrecibido == h.digest()):
+    cs.send(b"Recibido")
+    print("Recibido correctamente, el archivo esta completo y el Hash concuerda")
+else:
+    cs.send(b"noIntegridad")
+    print("La integridad se vio comprometida debido a que el Hash no concuerda")
 
 cs.shutdown(socket.SHUT_WR)
 cs.close()
 tiempoTransferencia= t2-t1
 print(tiempoTransferencia)
-sys.exit(0)
+try:
+    sys.exit(0)
+except:
+    print("Se apaga el cliente")
+
