2497ef3f6546c4b9aaab4006a9203f16ecf905e7
vianaernesto <le.viana@uniandes.edu.co>
2019-09-29 13:13:15 -0500

    Cliente con requerimientos
    Cumple con:
    1,2,3,5,6

--- app/app.py
+++ app/app.py
@@ -1,28 +1,47 @@
 import socket
 import time
+import hashlib
+import sys
 
 ipMaquinaServidor = "13.92.208.121"
-
-cs = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-##Para probar localmente cambiar "ipMaquinaServidor" por socket.gethostname()
-cs.connect((ipMaquinaServidor,5000))
-
 archivo = 'archivo.png'
+cs = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-cs.send(b"archivo2")
+print("Cliente iniciado")
+##Para probar localmente cambiar "ipMaquinaServidor" por socket.gethostname()
+cs.connect((socket.gethostname(),5000))
+print("Cliente conectandose")
+cs.send(b"syn")
+ack = cs.recv(1024)
+if(ack == b"ack"):
+    print("Cliente Conectado")
+    cs.send(b"Preparado")
+else:
+    print("Cliente no se pudo conectar")
+    cs.close()
+
 t1 = time.time()*1000
-
-
-
 print("Recibiendo..")
 with open(archivo, 'wb') as fw:
     while True:
-        data = cs.recv(1024)
-        if not data:
+        ##Esto es para que no sea bloqueante
+        cs.settimeout(0.1)
+        try:
+            data = cs.recv(1024)
+            fw.write(data)
+            if not data:
+                t2 = time.time()*1000
+                break
+        except Exception as e:
+            t2 = time.time()*1000
             break
-        fw.write(data)
     fw.close()
-    t2 = time.time()*1000
+cs.send(b"Recibido")
 print("Recibido")
-print(t2-t1)
+
+
+cs.shutdown(socket.SHUT_WR)
 cs.close()
+tiempoTransferencia= t2-t1
+print(tiempoTransferencia)
+sys.exit(0)
